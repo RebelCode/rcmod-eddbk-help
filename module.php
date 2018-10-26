00d420b63fe05c8419531b3f9ff746d28a2fcc2c
Miguel Muscat <miguelmuscat93@gmail.com>
2018-10-26 08:58:36 +0200

    Added module main file and class

---
+++ module.php
@@ -0,0 +1,20 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use RebelCode\EddBookings\Help\HelpModule;
+
+return function (ContainerInterface $c) {
+    return new HelpModule(
+        [
+            'key'                => 'eddbk_help',
+            'dependencies'       => [],
+            'config_file_path'   => __DIR__ . '/config.php',
+            'services_file_path' => __DIR__ . '/services.php',
+        ],
+        $c->get('config_factory'),
+        $c->get('container_factory'),
+        $c->get('composite_container_factory'),
+        $c->get('event_manager'),
+        $c->get('event_factory')
+    );
+};
